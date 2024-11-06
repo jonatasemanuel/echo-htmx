914cf22289c27723de1c30dfaa413b4df9af4715
Jonatas-Lap <jonatassilvaemanuel@gmail.com>
2024-11-06 17:08:15 -0300

    count app works
    adding htmx and style

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,27 +4,54 @@ import (
 	"time"
 
 	"github.com/a-h/templ"
-	"github.com/jonatasemanuel/templ-echo/internal/views"
+	"github.com/alexedwards/scs/v2"
+	views "github.com/jonatasemanuel/echo-htmx/internal/views/public"
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
-	e := echo.New()
-	e.GET("/", homeHandler)
-	e.GET("/time", timeHandler)
-	e.Logger.Fatal(e.Start(":3000"))
-}
-
-func homeHandler(c echo.Context) error {
+/* func homeHandler(c echo.Context) error {
 	return render(c, views.Home("joey"))
 }
 func timeHandler(c echo.Context) error {
 	return render(c, views.TimeComponent(time.Now()))
+} */
+
+type GlobalState struct {
+	Count int
 }
 
-func notFoundHandler(c echo.Context) error {
-	return render(c, views.NotFoundComponent())
+var global GlobalState
+var sessionManager *scs.SessionManager
+
+func getHandler(c echo.Context) error {
+	userCount := sessionManager.GetInt(c.Request().Context(), "count")
+	component := views.Page(global.Count, userCount)
+	return render(c, component)
 }
+
+func postHandler(c echo.Context) error {
+	if c.FormValue("global") != "" {
+		global.Count++
+	}
+	if c.FormValue("user") != "" {
+		currentCount := sessionManager.GetInt(c.Request().Context(), "count")
+		sessionManager.Put(c.Request().Context(), "count", currentCount+1)
+	}
+	return getHandler(c)
+}
+
 func render(ctx echo.Context, cmp templ.Component) error {
 	return cmp.Render(ctx.Request().Context(), ctx.Response())
+}
+
+func main() {
+	e := echo.New()
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+	e.Use(echo.WrapMiddleware(sessionManager.LoadAndSave))
+
+	e.GET("/", getHandler)
+	e.POST("/", postHandler)
+	e.Logger.Fatal(e.Start(":8080"))
+
 }
--- internal/views/public/layout_templ.go
+++ internal/views/public/layout_templ.go
@@ -42,7 +42,7 @@ func Layout(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title></head><body>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title><script src=\"/internal/views/public/htmx.min.js\"></script></head><body>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
