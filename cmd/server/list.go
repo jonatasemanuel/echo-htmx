daeae50496e1c8a4408e33cd55998855c2ab5c43
Jonatas-Lap <jonatassilvaemanuel@gmail.com>
2024-11-13 02:13:57 -0300

    working with maps to fetch list

--- cmd/server/list.go
+++ cmd/server/list.go
@@ -1,32 +1,5 @@
 package main
 
-func FetchQuestData() Quest {
-	return Quest{
-		Chars: []string{
-			"Ace", "Magna", "Gin", "Killer Bee", "Sato", "Air",
-			"Buchwald", "Bojack", "Walter", "Mikami", "Asada",
-			"Bulla", "Jose Porla", "Marlene", "Kotetsu",
-		},
-		Animes: []string{
-			"One Piece", "Naruto", "Bleach", "X-men",
-			"Black Clover", "Full Metal", "Attack on Titan", "HxH",
-			"HxH", "Naruto", "Yugi-Yo", "Full metal",
-			"Naruto", "One Piece", "Attack on Titan", "Bleach",
-			"My Hero Academia", "Dragon Ball Z", "Demon Slayer", "Naruto",
-			"One Punch Man", "Hunter x Hunter", "Naruto", "Death Note",
-			"Attack on Titan", "Demon Slayer", "Fullmetal Alchemist", "Naruto",
-			"Dragon Ball Z", "My Hero Academia", "Bleach", "Naruto",
-			"Black Clover", "One Punch Man", "Hunter x Hunter", "Naruto",
-			"Death Note", "One Piece", "Attack on Titan", "Fullmetal Alchemist",
-			"Sword Art Online", "Demon Slayer", "My Hero Academia", "Naruto",
-			"Dragon Ball Z", "Tokyo Ghoul", "One Punch Man", "Naruto",
-			"Fairy Tail", "Death Note", "Naruto", "Hunter x Hunter",
-			"Attack on Titan", "Naruto", "My Hero Academia", "Sword Art Online",
-			"Demon Slayer", "Tokyo Ghoul", "Bleach", "Dragon Ball Z",
-		},
-	}
-}
-
 const (
 	OnePiece           = "One Piece"
 	Naruto             = "Naruto"
